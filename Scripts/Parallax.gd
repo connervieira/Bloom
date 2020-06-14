eeb53c2e3c8c1828a88fd01508d98e0374998c2a
Conner Vieira <vieirast@icloud.com>
2020-06-14 19:17:08 -0400

    Added verticle offset to parallax

--- Scripts/Parallax.gd
+++ Scripts/Parallax.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 var CameraNode = Node
 
+var VerticalOffset = 300 #Determines how far down the parallax effect is moved in relation to the camera
+
 func _ready():
 	if get_tree().get_current_scene().get_name() == "World1":
 		CameraNode = get_tree().root.get_node("World1").get_node("Camera")
@@ -18,13 +20,13 @@ func _ready():
 
 func _process(delta):
 	self.get_child(0).position.x = CameraNode.position.x/2
-	self.get_child(0).position.y = CameraNode.position.y/4
+	self.get_child(0).position.y = (CameraNode.position.y/4)+300
 	
 	self.get_child(1).position.x = CameraNode.position.x/3
-	self.get_child(1).position.y = CameraNode.position.y/9
+	self.get_child(1).position.y = (CameraNode.position.y/9)+300
 	
 	self.get_child(2).position.x = CameraNode.position.x/5
-	self.get_child(2).position.y = CameraNode.position.y/25
+	self.get_child(2).position.y = (CameraNode.position.y/25)+300
 	
 	self.get_child(3).position.x = CameraNode.position.x/100
-	self.get_child(3).position.y = CameraNode.position.y/1000
+	self.get_child(3).position.y = (CameraNode.position.y/1000)+300
